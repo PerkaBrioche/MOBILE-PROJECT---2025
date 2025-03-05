6713c61897a7350f76ca92246e1fdc656066bce1
Lexisse <alexis.aillet@edu.devinci.fr>
2025-03-05 16:38:19 +0100

    Integrating [AUDIO] folder
    Title

--- Assets/[SCRIPTS]/IA/Enemy.cs
+++ Assets/[SCRIPTS]/IA/Enemy.cs
@@ -199,6 +199,7 @@ public class Enemy : MonoBehaviour
     #region MOVEMENT
     public void MoveInDirection(Func<TilesController, TilesController> direction, TilesController originTiles = null)
     {
+
         var listEnemyClosest = FindClosestEnemy();
         foreach (var pos in listEnemyClosest)
         {
@@ -216,7 +217,7 @@ public class Enemy : MonoBehaviour
         finalTile = originTiles;
         
         TilesController directionTile = direction(originTiles);
-        if (!IsTileValid(directionTile)) // BLOCKED AGAINST WALL
+        if (!IsTileValid(directionTile) || listEnemyClosest != null) // BLOCKED AGAINST WALL
         {
             for (int i = 1; i < listEnemyClosest.Count; i++)
             {
@@ -384,7 +385,6 @@ public class Enemy : MonoBehaviour
     
     if(shipDistances.Count <= 0)
         {
-            MoveInDirection(t => t.downTile);
             return null;
         }
     shipDistances.Sort((a, b) =>
